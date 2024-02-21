0505d7f091520901210a912c2414f61e412117f4
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-21 11:05:51 -0800

    s

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" City Module for HBNB project """
+import os
+from SQLAlchemy import Column, String
+from SQLAlchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from mdels.city import City
+import models
 
+class State(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'states'
+    name = Column(Strin(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """    """
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
