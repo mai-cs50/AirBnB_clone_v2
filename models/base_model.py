5ab7b2742dcc65b5149d4066fe93ab59a6686e97
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-21 10:23:47 -0800

    s

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,20 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
+            for key, value in kwargs.items():
+                if key != '__classes__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+                        # idd
+            if not hasattr(kwargs, 'id'):
+                setattr(self, 'id', str(uuid.uuid4()))
+            if not hasattr(kwargs, 'created_at'):
+                setattr(self, , 'created_at', datetime.now())
+            if not hasattr(kwargs, 'updated_at'):
+                setattr(self, 'updated_at', datetime.now())
+
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -37,14 +51,29 @@ class BaseModel:
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
+    def delete(self):
+        '''Delets BaseModel'''
+        from models import storage
+        storage.delete(self)
+
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
+        res = {}
+        for key, value in self.__dict.__items():
+            if key != '_sa_instance_state':
+                if isinstance(value, datetime):
+                    res[key] = value.isoformat()
+                else:
+                    res[key] = value
+        res['__class__'] = self.__class__.__name__
+        return res
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
