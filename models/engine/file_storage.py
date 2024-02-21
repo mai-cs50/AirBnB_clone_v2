b7353304b141c442b010403013d160e536f29046
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-21 11:22:43 -0800

    s

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def __init_(self):
+    '''def __init_(self):
         """   """
         self.model_classes = {
                 'BaseModel': import module('models.base_model').BaseModel,
@@ -20,7 +20,7 @@ class FileStorage:
                 'City': import module("models.city")City,
                 'Amenity': import module('models.amenity')Amenity,
                 'Review': import module("models.review")Review
-                }
+                }'''
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls = None:
@@ -36,8 +36,8 @@ class FileStorage:
         '''    '''
         if obj is not None:
             obj_key = obj.to_dict()['__class__'] + '.' + obj.id
-            if obj_key in self.__objects.keys()
-            del self.__objects[obj_key]
+            if obj_key in self.__objects.keys():
+                del self.__objects[obj_key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -57,12 +57,12 @@ class FileStorage:
     def reload(self):
         """Loads storage dictionary from file"""
         clsses = self.model_classes
-        if os.path.isfile(self.__file_path)
-        temp = {}
-        with open(self.__file_path, 'r') as file:
-            temp = json.load(file)
-            for key, val in temp.items():
-                self.all()[key] = classes[val['__classes__']](**val)
+        if os.path.isfile(self.__file_path):
+            temp = {}
+            with open(self.__file_path, 'r') as file:
+                temp = json.load(file)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__classes__']](**val)
 
     def close(self):
         '''  '''
