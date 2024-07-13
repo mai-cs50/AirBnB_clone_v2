f84905e31dd1995cdae6a390b7c7c15b86e40a1a
mai-cs50 <maimohamed.1992@yahoo.com>
2024-07-13 11:21:44 -0700

    d

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-#script that sets up your web servers for the deployment of web_static
+# Set up server file system for deployment
 
+# install nginx
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo service nginx start
+
+# configure file system
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# set permissions
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}n' /etc/nginx/sites-available/default
-sud service nginx restart
+
+# configure nginx
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# restart web server
+sudo service nginx restart
