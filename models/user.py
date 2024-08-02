35fa75c88e60bb1c3180de59e9e315c63fec41be
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-02 14:11:52 -0700

    s

--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-
-from SQLAlchemy import Column, String
-from SQLAlchemy.orm import relationship
+"""This is the user class"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = 'users'
-    email = Column(Strin(128), nullable=False)
-    password = relationship("City", backref="state", cascade="delete")
-    first_name = Column(Strin(128))
-    last_name = Column(Strin(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Place", backref="user", cascade="delete")
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    reviews = relationship('Review', cascade='all, delete', backref='user')
+    places = relationship('Place', cascade='all, delete', backref='user')
