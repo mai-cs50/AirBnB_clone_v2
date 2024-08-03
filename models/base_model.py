49f845d501d0a2102ef00541ee2fc7ddc3239e43
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-03 02:50:00 -0700

    s

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,6 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
-import pytz
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -24,8 +23,8 @@ class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.now(pytz.UTC))
-        updated_at = Column(DateTime, default=datetime.now(pytz.UTC))
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -34,20 +33,18 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time).replace
-                    (tzinfo=pytz.UTC)
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
-                self.created_at = datetime.now(pytz.UTC)
+                self.created_at = datetime.utcnow()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time).replace
-                    (tzinfo=pytz.UTC)
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
-                self.updated_at = datetime.now(pytz.UTC)
+                self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now((pytz.UTC))
+            self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
 
     def __str__(self):
@@ -57,7 +54,7 @@ class BaseModel:
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.utcnow(pytz.UTC)
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
